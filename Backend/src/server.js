75c04dda7299b61c38e32156ce3fe5ad4953f9d4
Bhabin Dulal <bhabindulal35@gmail.com>
2025-09-23 21:04:18 +0545

    backend completed

--- Backend/src/server.js
+++ Backend/src/server.js
@@ -2,23 +2,25 @@ import express from 'express';
 import notesRoutes from './routes/notesRoute.js'
 import { connectDB } from './config/db.js';
 import dotenv from'dotenv';
+import rateLimiter from './middleware/rateLimiter.js';
 
 dotenv.config();
 
 const app = express()
 const PORT = process.env.PORT || PORT
-connectDB();
+
 
 //middleware
 
 app.use(express.json());
+app.use(rateLimiter);
 app.use("/api/notes", notesRoutes)
 
-
-app.listen(PORT, ()=>{
+connectDB().then(()=>{
+    app.listen(PORT, ()=>{
     console.log("Server started on PORT:", PORT);
-});
+})
+})
 
 
 
-//mongodb+srv://bhabindulal:bhabinhero@cluster0.qfxfhpx.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0
