1be456b71290732e60064874998b19b38f967276
Bhabin Dulal <bhabindulal35@gmail.com>
2025-10-08 18:14:50 +0545

    creating note ui made

--- Frontend/src/pages/CreateNotes.jsx
+++ Frontend/src/pages/CreateNotes.jsx
@@ -1,11 +1,63 @@
-import React from 'react'
+import React from "react";
+import { useState } from "react";
+import { Link } from "react-router";
+import { ArrowLeftIcon } from "lucide-react";
 
 function CreateNotes() {
+  const [title, setTitle] = useState("");
+  const [content, setContent] = useState("");
+  const [loading, setLoading] = useState(false);
+
+  const handleSubmit = () => {};
   return (
-    <div>
-      
+    <div className="min-h-screen bg-base-200">
+      <div className="container mx-auto px-4 py-8">
+        <div className="max-w-2xl mx-auto">
+          <Link to={"/"} className="btn btn-ghost mb-6">
+            <ArrowLeftIcon className="size-5" />
+            Back to Notes
+          </Link>
+
+          <div className="card bg-base-100">
+            <div className="card-body">
+              <h2 className="card-title text-2xl mb-4"> Create New Note</h2>
+              <form onSubmit={handleSubmit}>
+                <div className="form-control mb-4 gap-3 flex flex-col">
+                  <label className="label">
+                    <span className="label-text">Title</span>
+                  </label>
+                  <input
+                    type="text"
+                    placeholder="Note Title"
+                    className="input"
+                    value={title}
+                    onChange={(e) => setTitle(e.target.value)}
+                  />
+                </div>
+                <div className="form-control mb-4 gap-3 flex flex-col">
+                  <label className="label">
+                    <span className="label-text">Content</span>
+                  </label>
+                  <textarea
+                    placeholder="Write Your Note Here..."
+                    className="textarea textarea-bordered h-32"
+                    value={content}
+                    onChange={(e) => setContent(e.target.value)}
+                  />
+                </div>
+
+                <div className="card-actions justify-end">
+                  <button type="submit" className="btn btn-primary" disabled={loading}>
+                    {loading? "Creating...": "Create Note"}
+                  </button>
+                </div>
+              </form>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
 
-export default CreateNotes
+export default CreateNotes;
